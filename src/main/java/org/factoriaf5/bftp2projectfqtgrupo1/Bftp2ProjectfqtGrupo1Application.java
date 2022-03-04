0f9d8b430ad3ff860eee93e65cc7429b2d293ee8
desimorenoh <desimorenoh@gmail.com>
2022-03-04 16:29:26 +0100

    first commit

---
+++ src/main/java/org/factoriaf5/bftp2projectfqtgrupo1/Bftp2ProjectfqtGrupo1Application.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.bftp2projectfqtgrupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bftp2ProjectfqtGrupo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bftp2ProjectfqtGrupo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/factoriaf5/bftp2projectfqtgrupo1/Bftp2ProjectfqtGrupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.bftp2projectfqtgrupo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bftp2ProjectfqtGrupo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
